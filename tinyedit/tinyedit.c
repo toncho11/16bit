0de4291c5afa9aa60406b5a024f4757cabbd7095
toncho11 <toncho11@gmail.com>
2020-12-19 08:58:03 +0100

    code improvements.

--- tinyedit/tinyedit.c
+++ tinyedit/tinyedit.c
@@ -2,11 +2,8 @@
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h> 
-
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
-
 #include <string.h>
 
 //#define clear() printf("\033[H\033[J")
@@ -16,8 +13,6 @@
 //printf("\033[XB"); // Move down X lines;
 //printf("\033[XC"); // Move right X column;
 //printf("\033[XD"); // Move left X column;
-
-
 #define SHOW_POS
 
 int curr_X = 1; //max 80
@@ -40,7 +35,7 @@ char getch(void)
     if(tcgetattr(0, &old) < 0)
         perror("tcsetattr()");
 
-    old.c_iflag &= ~(IXON); //disable Ctrl + S, Ctrl + Q
+    old.c_iflag &= ~(IXON); //release Ctrl + S, Ctrl + Q to be used in our program
 
     old.c_lflag &= ~ICANON;
     old.c_lflag &= ~ECHO;
@@ -67,35 +62,32 @@ char getch(void)
  
 int main(int argc, char *argv[])
 {
-	char* text = (char*)calloc(25 * 80, sizeof(char));
+	char* text = (char*)calloc(25 * 80, sizeof(char)); //Currently memory buffer is fixed to 2000!!!
+	if( !text ) {fputs("Error on memory allocation!",stderr); exit(1);}
 	
-    //printf("Reading file...");
-    //sprintf(text, "%s","Enter your number in the box below\n+-----------------+\n|                 |\n+-----------------+\n");
 	if ( argc != 2 )
     {
         printf( "usage: %s filename", argv[0] );
     }
     else
     {
-        // We assume argv[1] is a filename to open
+		//printf("Reading file...");
+			
         FILE *file = fopen( argv[1], "r" );
 
-        /* fopen returns 0, the NULL pointer, on failure */
         if ( file == 0 )
         {
-            printf( "Could not open file\n" );
+            printf( "Error opening file for read\n" );
 			exit(1);
         }
         else
         {
 			int fd = fileno(file); 
+			
+			//get file size
 			struct stat buf;
 			fstat(fd, &buf);
 			text_size = buf.st_size;
-
-            // fseek( file , 0L , SEEK_END);
-            // int lSize = ftell( file );
-            // rewind( file ); //fseek(fp, 0L, SEEK_SET);
 
 			/* allocate memory for entire content */
 			//buffer = calloc( 1, lSize+1 );
@@ -122,7 +114,7 @@ int main(int argc, char *argv[])
 		if (text[i] == '\n')
 		{
 			line_ends[line] = i;
-			//printf("\n %d",line_ends[line]);
+			//printf("\n %d",line_ends[line]); //for debug
 			line++;
 			
 		}
@@ -130,7 +122,7 @@ int main(int argc, char *argv[])
 	for (i = line; i< 26; i++) line_ends[i] = -1;
 	
 	if (line_ends[0] == '\r') is_linux_file = 0; //0 means DOS file
-	//printf("\n %d\n",is_linux_file);
+	//printf("\n %d\n",is_linux_file); //for debug
 	
 	printf("%s",text);
 	
@@ -149,10 +141,8 @@ int main(int argc, char *argv[])
             getch(); // skip the [
 			char c = getch();
 			
-			//int x_now = line_ends[curr_Y-1] + curr_X;
-			
 			switch(c)
-			{ // the real value
+			{ // the real value that gives the direcion
 				case 'A': // code for arrow up - OK
 
 				    //printf("%s","A");
@@ -177,15 +167,13 @@ int main(int argc, char *argv[])
 					break;
 				case 'B': // code for arrow down - OK
 					
-					//printf("%s","B");
-					//printf("\033[1B"); // Move down X lines;
-					
+					//printf("%s","B");					
 					if (curr_Y<26 && line_ends[curr_Y+1] != -1)
 					{
+						//line below current one is shorter: we move down, but we also move right to the end of the next line
 						if (line_ends[curr_Y + 1] < line_ends[curr_Y] + curr_X)
 						{
-							//curr_X = line_ends[curr_Y + 1] - line_ends[curr_Y] ;
-							curr_X = line_ends[curr_Y + 1] - line_ends[curr_Y] ;
+							curr_X = line_ends[curr_Y + 1] - line_ends[curr_Y]; //calculates the end of the next line (curr_Y+1)
 						}
 						
 						curr_Y = curr_Y + 1;
@@ -200,7 +188,8 @@ int main(int argc, char *argv[])
 				case 'C': // code for arrow right
 					
 					//printf("%s","C");
-					if (line_ends[curr_Y - 1] + curr_X == line_ends[curr_Y] && line_ends[curr_Y+1] != -1 ) //we are currently at the new line and the user clicked to go right
+					//we are currently at the new line (at the very right position) and the user clicked to go right: we move down
+					if (line_ends[curr_Y - 1] + curr_X == line_ends[curr_Y] && line_ends[curr_Y+1] != -1 )
                     {
 						if (curr_Y<25) curr_Y = curr_Y + 1;
 						curr_X = 1;
@@ -223,6 +212,7 @@ int main(int argc, char *argv[])
 				case 'D': // code for arrow left - OK
 					
 					//printf("%s","D");
+					//we are at the very left position and the user clicked left: we move up
 					if (curr_X == 1 && curr_Y > 1)
                     {
 						curr_X = line_ends[curr_Y-1] - line_ends[curr_Y-2];
@@ -245,7 +235,7 @@ int main(int argc, char *argv[])
 			}
 	   }
 	   else 
-	   if (curr_ch == '\n' || curr_ch == '\r') //ubuntu works with \n and ELKS works with \r
+	   if (curr_ch == '\n' || curr_ch == '\r') // Handles "enter" , ubuntu works with \n and ELKS works with \r
 	   {
 		   //printf("%s", "enter");
 		   printf("%s", "\r\n");
@@ -256,45 +246,47 @@ int main(int argc, char *argv[])
 		   printf("[%d | %d]  ",curr_X,curr_Y);
 		   gotoxy(curr_X, curr_Y);
 	   }
-           else if (curr_ch == 127 || curr_ch == '\b') //detect backspace (127 on Ubuntu, '\b' on ELKS) and move text right
-           {
-             if (curr_X != 1)
-             { 
-		       int start_text_pos = line_ends[curr_Y-1] + curr_X;
-               memcpy(text + start_text_pos - 1, text + start_text_pos, text_size - start_text_pos);
+	   else if (curr_ch == 127 || curr_ch == '\b') //detect backspace (127 on Ubuntu, '\b' on ELKS) and move text right
+	   {
+		 if (curr_X != 1)
+		 { 
+		   int start_text_pos = line_ends[curr_Y-1] + curr_X;
+		   memcpy(text + start_text_pos - 1, text + start_text_pos, text_size - start_text_pos);
 
-               //add extra space to delete last character
-               text[line_ends[curr_Y] - 1] = ' ';
-			   char c = text[line_ends[curr_Y]];
-               text[line_ends[curr_Y]] = '\0';
+		   //add extra space to delete last character
+		   text[line_ends[curr_Y] - 1] = ' ';
+		   char c = text[line_ends[curr_Y]];
+		   text[line_ends[curr_Y]] = '\0';
 
-               //print at new position
-			   gotoxy(curr_X - 1, curr_Y);
-               printf("%s", text + start_text_pos - 1);
-               
-               //remove space
-               text[line_ends[curr_Y] -1 ] = '\n'; //put the new end of line
-			   text[line_ends[curr_Y]] = c;
-               
-			   curr_X = curr_X - 1;
-               gotoxy(curr_X, curr_Y);
-               
-               for (int i=curr_Y; i< 26; i++)
-				  line_ends[i]--; 
-			   
-			   text_size--;
-			   text[text_size]='\0';
-             }
-			 else
-			 {
-			   int start_text_pos = line_ends[curr_Y-1];
-               memcpy(text + start_text_pos, text + start_text_pos, text_size - start_text_pos);
-			 }
-           } 
-	   else //print character and move all characters right
+		   //print at new position
+		   gotoxy(curr_X - 1, curr_Y);
+		   printf("%s", text + start_text_pos - 1);
+		   
+		   //remove space
+		   text[line_ends[curr_Y] -1 ] = '\n'; //put the new end of line
+		   text[line_ends[curr_Y]] = c;
+		   
+		   curr_X = curr_X - 1;
+		   gotoxy(curr_X, curr_Y);
+		   
+		   for (int i=curr_Y; i< 26; i++)
+			  if (line_ends[i] != -1) line_ends[i]--; 
+		   
+		   text_size--;
+		   text[text_size]='\0';
+		 }
+		 else
+		 {
+		   int start_text_pos = line_ends[curr_Y-1];
+		   memcpy(text + start_text_pos, text + start_text_pos, text_size - start_text_pos);
+		 }
+	   } //end backspace 
+       else 
 	   if (curr_ch == 17) //CTRL + Q
 	   {
-		   printf("%s", "CTRL + Q");
+		   //printf("%s", "CTRL + Q");
+		   //TODO: must go to the first unused line to put the prompt
+		   exit(0);
 	   }
 	   else 
 	   if (curr_ch == 19) //CTRL + S 
@@ -304,50 +296,49 @@ int main(int argc, char *argv[])
 		    FILE *f = fopen("file2.txt", "w");
 			if (f == NULL)
 			{
-				printf("Error opening file!\n");
+				printf("Error opening file for write!\n");
 				exit(1);
 			}
 
-			/* print some text */
 			fprintf(f, "%s", text);
 			
-			//debug 
+			//start debug 
 			fprintf(f, "\ntext size %d", text_size);
 			fprintf(f, "\nline1 %d", line_ends[1]);
 			fprintf(f, "\nline2 %d", line_ends[2]);
 			fprintf(f, "\nline3 %d", line_ends[12]);
+			//end debug
 			
-
 			fclose(f);
 	   }
 	   else //add character and move text right
-           {
-             if (curr_X != 80)
-             {  
-  		       int start_text_pos = line_ends[curr_Y-1] + curr_X;
-			   memcpy(text + start_text_pos + 1, text + start_text_pos, text_size - start_text_pos);
+	   {
+		 if (curr_X != 80)
+		 {  
+		   int start_text_pos = line_ends[curr_Y-1] + curr_X;
+		   memcpy(text + start_text_pos + 1, text + start_text_pos, text_size - start_text_pos);
 
-			   //if (curr_X == 1) line_ends[i] == //PROBLEM
-			   for (int i=curr_Y; i< 26; i++)
-				   if (line_ends[i]!= -1) line_ends[i]++; 
-			   
-               //add character
-               text[start_text_pos] = curr_ch;
-               text[line_ends[curr_Y]] = '\0'; //to be able to print and then it will be switched to \n 
+		   //if (curr_X == 1) line_ends[i] == //PROBLEM
+		   for (int i=curr_Y; i< 26; i++)
+			   if (line_ends[i]!= -1) line_ends[i]++; 
+		   
+		   //add character
+		   text[start_text_pos] = curr_ch;
+		   text[line_ends[curr_Y]] = '\0'; //to be able to print and then it will be switched to \n 
 
-               //print at new position
-               printf("%s", text + start_text_pos);
-			   
-			   text[line_ends[curr_Y]] = '\n';
+		   //print at new position
+		   printf("%s", text + start_text_pos);
+		   
+		   text[line_ends[curr_Y]] = '\n';
 
-			   curr_X = curr_X + 1;
-               gotoxy(curr_X, curr_Y);
-               
-			   text_size++;
-			   text[text_size]='\0';
-             }
-           } 
-    }
+		   curr_X = curr_X + 1;
+		   gotoxy(curr_X, curr_Y);
+		   
+		   text_size++;
+		   text[text_size]='\0';
+		 }
+	   } //end add character
+    } //end while
 	
     return 0;
 }
